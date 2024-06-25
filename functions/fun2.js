254f6055bc374f7cdf57cbbd33d5a62d8fa29bd7
Dina <dinanath88090@outlook.com>
2024-06-25 07:45:27 +0530

    funtions

---
+++ functions/fun2.js
@@ -0,0 +1,12 @@
+// Function Expressions: Learn about anonymous functions and arrow functions
+
+let add=function(a,b){
+    return a+b;
+}
+console.log(add(12,3));
+
+
+let subtraction=function(x,y){
+    return x-y;
+}
+console.log(`Subtraction of x and y is = `+subtraction(20,18));
